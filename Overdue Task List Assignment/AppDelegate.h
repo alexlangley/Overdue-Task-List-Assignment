81fa5acd7067505b4c6cd8b863e74fa3212740aa
alexlangley <msg2alex@gmail.com>
2014-10-08 16:30:36 -0500

    Initial Commit

---
+++ Overdue Task List Assignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Overdue Task List Assignment
+//
+//  Created by Alex Langley on 10/8/14.
+//  Copyright (c) 2014 Truu Bruu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Overdue Task List Assignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Overdue Task List Assignment
+//
+//  Created by Alex Langley on 10/8/14.
+//  Copyright (c) 2014 Truu Bruu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
